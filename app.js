d72de315980997260347f3b9e21bbb24cc68997a
c4171208 <c4171208@gmail.com>
2024-03-05 10:17:23 +0200

    step2

---
+++ app.js
@@ -0,0 +1,13 @@
+// import {config} from "dotenv"
+import userRouter from "./router/user.js"
+import cors from "cors";
+
+// config();
+const app=express()
+
+
+
+app.use(express.json());
+app.use(cors({origin:"*",methods:"*"}))
+
+app.use("/api/user",userRouter)
